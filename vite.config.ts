aca07a8fe002ab330033a3c4c18106f44b10f449
NiyaazZSB <133034977+NiyaazZSB@users.noreply.github.com>
2025-06-24 15:57:55 +0200

    reverted

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace this with your actual repo name
-const repoName = "theme-craft-resume-ai";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`, // 👈 required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
